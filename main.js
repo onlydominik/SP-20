17da6cd7cd635d791ad5ba39eca66532556b35fa
Dominik Kowalczyk <kdominikpl@gmail.com>
2024-08-30 23:13:36 +0200

    FETCH API AND PROMISES LEARNING

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-let data = fetch("data.json");
+function f() {
+  try {
+    alert("start");
+    throw new Error("an error");
+  } catch (err) {
+    // ...
+    if ("can't handle the error") {
+      throw err;
+    }
+  }
+}
 
-console.log(data)
+f();
+alert("cleanup!");
+
+//FETCH API AND PROMISES LEARNING IN BACKGROUND
